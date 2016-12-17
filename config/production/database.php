4879f9a8db1bb4a3ed38f3aa9ca992c361671b32
Angela Adams <aadams3867@gmail.com>
2016-12-17 01:04:26 -0500

    Update database config for production env

---
+++ config/production/database.php
@@ -0,0 +1,30 @@
+<?php
+
+/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysql($server, $username, $password, $db);*/
+
+/*return [
+
+    'connections' => [
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+    ],
+];*/
